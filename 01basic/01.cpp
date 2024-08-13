a91b654b81d9975b68532d71d6c8c9dd387c55b3
Basu Slathia <basuslathia01@gmail.com>
2024-08-13 23:31:58 +0530

    Create 01.cpp

---
+++ 01basic/01.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std; 
+int main()
+{
+    int a;
+    cout<<12;
+
+}
+
+
+
+
+#include<iostream>
+using namespace std; 
+int main()
+{
+    int a;
+    cin>>a;
+    cout<< "HEllo this is \n";
+    cout<<a;
+}
+
