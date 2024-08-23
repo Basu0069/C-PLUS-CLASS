dcb6e56c9fb0ef0787344056c9c3cd7c240fabf7
Basu Slathia <basuslathia01@gmail.com>
2024-08-23 23:46:19 +0530

    Create 02_string .cpp

---
+++ 01basic/02_string .cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    string name;
+    cin>>name;
+    char a;
+    cin>>a;
+    int count=0;
+    for(int i=0;i<name.length();i++){
+        if(name[i]==a){
+            // count++;
+            cout<<name[i];
+        }
+    };
+    
+// cout<<"The letter appears that times "<<count;
+}
