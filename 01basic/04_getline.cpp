f1f50eba18079efa93f00de005323ed60eb25204
Basu Slathia <basuslathia01@gmail.com>
2024-09-12 17:01:52 +0530

    Create 04_getline.cpp

---
+++ 01basic/04_getline.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    string c,f;
+    getline(cin,c);
+    cin.ignore();
+    getline(cin,f);
+    cout<<c;
+    cout<<endl;
+    cout<<f;
+}
